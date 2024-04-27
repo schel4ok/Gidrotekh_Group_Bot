839cf16dd1ebf4e0794c5244a0486d1ff930c40e
user530 <m.timarsuev@gmail.com>
2024-04-27 23:55:58 +0300

    Get keyboard utility function

---
+++ utils/getKeyboard.ts
@@ -0,0 +1,9 @@
+import { Markup } from 'telegraf';
+
+export function getKeyboard(options: Record<string, string>) {
+    return Markup.inlineKeyboard(
+        Object.entries(options).map(
+            (([text, nextNode]) => Markup.button.callback(text, nextNode))
+        )
+    )
+}
