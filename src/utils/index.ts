8fc7885f8d9d275a8446a39b2adf8a5b2f03bee1
user530 <m.timarsuev@gmail.com>
2024-05-06 18:24:30 +0300

    Updated validation utils and added barrel file

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { getKeyboard } from './getKeyboard';
+export { loadJsonData } from './loadData';
+export { createContactsInput, validateCompany, validateName, validatePhone } from './validateConctacts';
+export { validateNodeFlow } from './validateNodes';
--- src/utils/validateConctacts.ts
+++ src/utils/validateConctacts.ts
@@ -1,26 +1,24 @@
 import { UserContactsInput } from '../interfaces';
 
-export function createContactsInput(inputText: string): UserContactsInput {
-    try {
-        const parsedInputs = inputText.split(',');
-        return {
-            name: parsedInputs[0].trim(),
-            company: parsedInputs[1].trim(),
-            phone: parsedInputs[2].trim()
-        };
-    } catch (error) {
-        return {
-            name: '',
-            company: '',
-            phone: ''
-        }
+export function createContactsInput(): UserContactsInput {
+    return {
+        name: '',
+        company: '',
+        phone: ''
     }
 }
 
-export function validateContactsInput(input: UserContactsInput): boolean {
-    return (
-        input.name.length > 0
-        && input.company.length >= 0
-        && (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(input.phone))
-    );
+export function validateName(name: unknown): boolean {
+    return typeof name === 'string'
+        && name.length > 0;
+}
+
+export function validateCompany(company: unknown): boolean {
+    return typeof company === 'string'
+        && company.length > 0;
+}
+
+export function validatePhone(phone: unknown): boolean {
+    return typeof phone === 'string'
+        && (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(phone))
 }
